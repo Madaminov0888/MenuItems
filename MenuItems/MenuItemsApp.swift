47c80b3bec5d847e7d536f3d753536e91a17ee61
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2025-01-19 19:35:13 +0500

    Initial Commit

---
+++ MenuItems/MenuItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItemsApp.swift
+//  MenuItems
+//
+//  Created by Muhammadjon Madaminov on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
